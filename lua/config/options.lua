3e357024f3ce1837214fa0bdae56bcbd7bb0dffb
Helmy Saker <helmy93@yandex.com>
2024-12-09 13:45:39 -0500

    ...

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,6 +15,9 @@ vim.diagnostic.config({
 })
 
 if vim.g.vscode then
-    local vscode = require('vscode')
-    vim.notify = vscode.notify
+    local vscode, _  = pcall(require, "vscode")
+
+    if vscode then
+        vim.notify = vscode.notify
+    end
 end
