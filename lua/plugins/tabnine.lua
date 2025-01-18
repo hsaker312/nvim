7174c29ce075db428ed6ea22cc954bbb0f2e70e3
Helmy Saker <helmy93@yandex.com>
2025-01-18 16:35:35 -0500

    ...

--- lua/plugins/tabnine.lua
+++ lua/plugins/tabnine.lua
@@ -1,7 +1,7 @@
 return {
     "codota/tabnine-nvim",
     enabled = not vim.g.lite,
-    build = vim.g.windows and "pwsh.exe -file .\\dl_binaries.ps1" or "./dl_binaries.sh",
+    build = vim.g.windows and "powershell.exe -file .\\dl_binaries.ps1" or "./dl_binaries.sh",
     config = function()
         require("tabnine").setup({
             disable_auto_comment = true,
