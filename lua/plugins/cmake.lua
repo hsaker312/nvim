17ee87deb978065e32ce63d6cd3c5e8117cd6748
Helmy Saker <helmy93@yandex.com>
2025-05-22 19:22:35 -0400

    ...

--- lua/plugins/cmake.lua
+++ lua/plugins/cmake.lua
@@ -1,4 +1,8 @@
 return {
     "Civitasv/cmake-tools.nvim",
     enabled = not vim.g.lite,
+    commit = "3637e4cb3289af7d7de2ea6cadfd2387d669d04b",
+    checker = {
+        enabled = false, -- disable the checker
+    },
 }
