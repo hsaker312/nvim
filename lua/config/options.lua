845b5baf174cfddd93c5df5ba3e97cfd95ccd977
Helmy Saker <hsaker@meso-scale.com>
2025-07-17 18:06:06 -0400

    ...

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,7 +16,19 @@ vim.g.snacks_animate = false
 vim.opt.scrolloff = 10
 vim.opt.mousemoveevent = true
 
-vim.opt.clipboard = 'unnamedplus'
+-- vim.opt.clipboard = 'unnamedplus'
+
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+    ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+    ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+  },
+}
 
 vim.diagnostic.config({
     virtual_text = false,
